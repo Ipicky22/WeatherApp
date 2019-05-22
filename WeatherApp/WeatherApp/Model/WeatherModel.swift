26bfc6cf282d68ff5cb1be1488e37a42f21d559a
Adrien <adrien.masson@hotmail.fr>
2019-05-22 09:02:31 +0200

    Model & Storyboard

---
+++ WeatherApp/WeatherApp/Model/WeatherModel.swift
@@ -0,0 +1,23 @@
+//import Foundation
+//
+//struct WeatherModel : Decodable {
+//
+//    enum Codingkeys: String, Codingkey {
+//        case currentlyIcon = "icon"
+//        case currentlyTemperature = "temperature"
+//        case currentlySummary = "summary"
+//        case currentlyWindSpeed = "windSpeed"
+//        case currentlyPressure = "pressure"
+//        case currentlyHumidity = "humidity"
+//        case currentlyUvIndex = "uvIndex"
+//    }
+//
+//    var currentlyIcon : String
+//    var currentlyTemperature : String
+//    var currentlySummary : String
+//    var currentlyWindSpeed : String
+//    var currentlyPressure : String
+//    var currentlyHumidity : String
+//    var currentlyUvIndex : String
+//}
+//
