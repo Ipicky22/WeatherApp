6e534103b10e3aceb69c8711ce3f99e9e25760dd
Gouénard Maxime <maxime.gouenard@gmail.com>
2019-05-22 22:48:30 +0200

    list city map

--- WeatherApp/WeatherApp/Controller/MapViewController.swift
+++ WeatherApp/WeatherApp/Controller/MapViewController.swift
@@ -2,14 +2,34 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, UITableViewDataSource {
+    
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var button: UIBarButtonItem!
+    
+    var cities: [City] = CitiesData.list
+    
+    @IBAction func pressButton(_ sender: Any) {
+        
+        if tableView.isHidden == true {
+            tableView.isHidden = false
+            mapView.isHidden = true
+            
+        } else {
+            mapView.isHidden = false
+            tableView.isHidden = true
+        }
+    }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         mapView.delegate = self
+        tableView.dataSource = self
         
         for city in CitiesData.list {
             let pin = MKPointAnnotation()
@@ -17,6 +37,12 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             pin.title = city.name
             mapView.addAnnotation(pin)
         }
+        
+        setUpTableView()
+    }
+    
+    func setUpTableView() {
+        tableView.register(UINib(nibName: "CityTableViewCell", bundle: nil), forCellReuseIdentifier: "CityTableViewCell_ID")
     }
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
@@ -31,6 +57,18 @@ class MapViewController: UIViewController, MKMapViewDelegate {
                 detailsWeather.annotationDetail = sender as? MKPointAnnotation
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cellCity = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell_ID", for: indexPath) as? CityTableViewCell {
+            cellCity.configure(city: cities[indexPath.row].name)
+            return cellCity
+        }
+        return UITableViewCell()
     }
     
     
