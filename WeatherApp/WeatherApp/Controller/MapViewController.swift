7df5cd3b3db1fca5400b6b3faa0021546b823ac8
Adrien <adrien.masson@hotmail.fr>
2019-05-22 09:06:14 +0200

    Class StoryBoard

--- WeatherApp/WeatherApp/Controller/MapViewController.swift
+++ WeatherApp/WeatherApp/Controller/MapViewController.swift
@@ -1,30 +1,16 @@
-//
-//  MapViewController.swift
-//  WeatherApp
-//
-//  Created by Adrien Masson on 22/05/2019.
-//  Copyright © 2019 Adrien. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
