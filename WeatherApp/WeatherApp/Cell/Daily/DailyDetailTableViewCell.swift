64211b12d4c59a87a487d2659f6bab8f20b7e32f
Adrien <adrien.masson@hotmail.fr>
2019-05-22 08:50:52 +0200

    Structure Project

---
+++ WeatherApp/WeatherApp/Cell/Daily/DailyDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyDetailTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Adrien Masson on 22/05/2019.
+//  Copyright © 2019 Adrien. All rights reserved.
+//
+
+import UIKit
+
+class DailyDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherApp/WeatherApp/Cell/Daily/DailyHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyHeaderTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Adrien Masson on 22/05/2019.
+//  Copyright © 2019 Adrien. All rights reserved.
+//
+
+import UIKit
+
+class DailyHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
